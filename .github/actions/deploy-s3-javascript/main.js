ecf2649a3798fadd2b2d9435e8b9edce2e658368
Harsha Reddy <harsha.reddy@sap.com>
2026-06-07 20:28:12 -0400

    add custom deploy action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,6 +3,11 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run(){
+ const bucket = core.getInput('bucket',{required:true});
+ const bucketregion = core.getInput('bucket-region',{required:true});
+ const distFolder = core.getInput('dist-folder',{required:true});
+
+ exec.exec(`aws s3 sync ${distFolder} s3://${bucket} --region ${bucketregion}` );
  core.notice('Hello from my custom JavaScript action!');
 }
 
