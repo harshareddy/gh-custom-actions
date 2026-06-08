e899439264d8b887819bc857431decf7abd1a0bc
Harsha Reddy <harsha.reddy@sap.com>
2026-06-07 20:47:47 -0400

    add aws s3 deploy

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -9,6 +9,10 @@ function run(){
 
  exec.exec(`aws s3 sync ${distFolder} s3://${bucket} --region ${bucketregion}` );
  core.notice('Hello from my custom JavaScript action!');
+
+ const websiteUrl = `http://${bucket}.s3-website-${bucketregion}.amazonaws.com`;
+ core.notice(`Your website is deployed at ${websiteUrl}`);
+ core.setOutput('website-url', websiteUrl);
 }
 
 run();
